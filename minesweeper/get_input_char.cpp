be98b00efa2b22a5d2e62cbfdcc16227c607adb0
Константин Столяров <k.stolyarov@2gis.ru>
2016-01-23 18:32:16 +0600

    Added function for reading input characters

---
+++ minesweeper/get_input_char.cpp
@@ -0,0 +1,35 @@
+#include "get_input_char.h"
+
+#if defined(_WIN32) || defined(WIN32)
+#include <conio.h>
+char get_input_char()
+{
+	return static_cast<char>(getch());
+}
+
+#else // linux, mac
+#include <stdio.h>
+#include <unistd.h>
+#include <termios.h>
+
+char get_input_char()
+{
+        char buf = 0;
+        struct termios old = {0};
+        if (tcgetattr(0, &old) < 0)
+                perror("tcsetattr()");
+        old.c_lflag &= ~ICANON;
+        old.c_lflag &= ~ECHO;
+        old.c_cc[VMIN] = 1;
+        old.c_cc[VTIME] = 0;
+        if (tcsetattr(0, TCSANOW, &old) < 0)
+                perror("tcsetattr ICANON");
+        if (read(0, &buf, 1) < 0)
+                perror ("read()");
+        old.c_lflag |= ICANON;
+        old.c_lflag |= ECHO;
+        if (tcsetattr(0, TCSADRAIN, &old) < 0)
+                perror ("tcsetattr ~ICANON");
+        return (buf);
+}
+#endif
---
+++ minesweeper/get_input_char.h
@@ -0,0 +1,7 @@
+#ifndef GET_INPUT_CHAR_H
+#define GET_INPUT_CHAR_H
+
+// Read next input char from a keyboard. Call is blocked until character is received.
+char get_input_char();
+
+#endif // GET_INPUT_CHAR_H
