d52411f0f0a5262f6d2ed85fc4327f25d08e827b
Konstantin A. Stolyaov <knibbler@gmai.com>
2016-01-23 12:23:16 +0600

    initial menu printing

--- minesweeper/main.cpp
+++ minesweeper/main.cpp
@@ -1,24 +1,62 @@
 #include <cstdlib>
 #include <iostream>
+#include <stdio.h>
 #include "minesweeper.h"
 using namespace std;
 
 void playTime(minesweeper& play);
 void viewStatistics();
 
-/*
- * 
- */
+const char * NORMAL_TEXT = "\x1B[0m";
+const char * BOLD_RED_TEXT = "\x1B[1;31m";
+const char * BOLD_BRIGHT_RED_TEXT = "\x1B[1;38m";
+const char * BOLD_GREEN_TEXT = "\x1B[1;32m";
+const char * BOLD_BRIGHT_GREEN_TEXT = "\x1B[1;39m";
+const char * BOLD_YELLOW_TEXT = "\x1B[1;33m";
+const char * BOLD_BRIGHT_YELLOW_TEXT = "\x1B[1;3Am";
+const char * BOLD_BLUE_TEXT = "\x1B[1;34m";
+const char * BOLD_BRIGHT_BLUE_TEXT = "\x1B[1;3Bm";
+const char * BOLD_MAGENTA_TEXT = "\x1B[1;35m";
+const char * BOLD_BRIGHT_MAGENTA_TEXT = "\x1B[1;3Cm";
+const char * BOLD_CYAN_TEXT = "\x1B[1;36m";
+const char * BOLD_BRIGHT_CYAN_TEXT = "\x1B[1;3Dm";
+const char * BOLD_WHITE_TEXT = "\x1B[1;37m";
+const char * BOLD_BRIGHT_WHITE_TEXT = "\x1B[1;3Em";
+
+const char exit_action_key = 'q';
+const char start_game_action_key = 'n';
+const char load_game_action_key = 'l';
+const char view_stats_action_key = 's';
+
+void print_initial_actions()
+{
+	// iostream manipulations to get colored output is stupid. Use old-good printf instead.
+	const char * initial_actions_header =
+		"This is main menu for Minesweeper game.\n"
+		"Possible actions are:";
+	const char * action_prefix = "\t Press";
+	const char * exit_game_action_msg = "to exist the game.";
+	const char * start_game_action_msg = "to start new game.";
+	const char * load_game_action_msg = "to load saved game from file.";
+	const char * view_stats_action_msg = "to view statistics.";
+
+	printf("%s\n", initial_actions_header);
+	printf("%s '%s%c%s' %s\n", action_prefix, BOLD_BRIGHT_RED_TEXT, exit_action_key, NORMAL_TEXT, exit_game_action_msg);
+	printf("%s '%s%c%s' %s\n", action_prefix, BOLD_BRIGHT_RED_TEXT, start_game_action_key, NORMAL_TEXT, start_game_action_msg);
+	printf("%s '%s%c%s' %s\n", action_prefix, BOLD_BRIGHT_RED_TEXT, load_game_action_key, NORMAL_TEXT, load_game_action_msg);
+	printf("%s '%s%c%s' %s\n", action_prefix, BOLD_BRIGHT_RED_TEXT, view_stats_action_key, NORMAL_TEXT, view_stats_action_msg);
+}
+
 int main() {
 
-    
-     /* This will be your main GUI loop. You should query the user for actions such 
+	/* This will be your main GUI loop. You should query the user for actions such 
      * as starting a game, viewing statistics, or quitting the program. You should
      * handle user input either succinctly or by making a call to a helper method
      * to perform the desired operations.
      */
+	print_initial_actions();
     while (1) {
-
+		
         
 	//TODO - Ask the user with options to perform such as Starting a game, Viewing
 	// statistics, Quitting the program,  and any 
