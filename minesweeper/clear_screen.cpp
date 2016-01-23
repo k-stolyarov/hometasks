2c6b799b075daf209adfc52324dbc9bd3d3fef5a
Konstantin A. Stolyaov <knibbler@gmai.com>
2016-01-23 16:36:38 +0600

    Added screen clearing function

---
+++ minesweeper/clear_screen.cpp
@@ -0,0 +1,57 @@
+#include "clear_screen.h"
+
+#if defined(_WIN32) || defined(WIN32)
+#include <windows.h>
+
+void clear_screen()
+{
+	HANDLE                     hStdOut;
+	CONSOLE_SCREEN_BUFFER_INFO csbi;
+	DWORD                      count;
+	DWORD                      cellCount;
+	COORD                      homeCoords = { 0, 0 };
+
+	hStdOut = GetStdHandle(STD_OUTPUT_HANDLE);
+	if (hStdOut == INVALID_HANDLE_VALUE) return;
+
+	/* Get the number of cells in the current buffer */
+	if (!GetConsoleScreenBufferInfo(hStdOut, &csbi)) return;
+	cellCount = csbi.dwSize.X *csbi.dwSize.Y;
+
+	/* Fill the entire buffer with spaces */
+	if (!FillConsoleOutputCharacter(
+		hStdOut,
+		(TCHAR) ' ',
+		cellCount,
+		homeCoords,
+		&count
+		)) return;
+
+	/* Fill the entire buffer with the current colors and attributes */
+	if (!FillConsoleOutputAttribute(
+		hStdOut,
+		csbi.wAttributes,
+		cellCount,
+		homeCoords,
+		&count
+		)) return;
+
+	/* Move the cursor home */
+	SetConsoleCursorPosition(hStdOut, homeCoords);
+}
+#else // linux, mac
+#include <unistd.h>
+#include <term.h>
+
+void clear_screen()
+{
+	if (!cur_term)
+	{
+		int result;
+		setupterm(NULL, STDOUT_FILENO, &result);
+		if (result <= 0) return;
+	}
+
+	putp(tigetstr("clear"));
+}
+#endif
---
+++ minesweeper/clear_screen.h
@@ -0,0 +1,7 @@
+#ifndef CLEAR_SCREEN_H
+#define CLEAR_SCREEN_H
+
+// Auxiliary function that clear terminal to print from the top-left position
+void clear_screen();
+
+#endif // #ifndef CLEAR_SCREEN_H
